f89019bd5a23e3ef832ef1735329e655c845622f
IntelligenceHubCreates <intelligencehub20@gmail.com>
2025-10-09 17:32:10 +0530

    hostinger commit

---
+++ out/_next/static/chunks/2457bf031134937d.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/0a86c048ea9b26e3.js",
+  "static/chunks/turbopack-e247e5768a626aa9.js"
+])
---
+++ out/_next/static/chunks/40ca85cc08f9178e.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/0a86c048ea9b26e3.js",
+  "static/chunks/turbopack-1ba35861b9088763.js"
+])
